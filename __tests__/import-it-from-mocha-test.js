bd892d248e544acdc34d28999122c451def5a9fa
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-24 16:26:44 +0100

    Add "test-helper" test

--- __tests__/import-it-from-mocha-test.js
+++ __tests__/import-it-from-mocha-test.js
@@ -3,6 +3,7 @@
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
 defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/basic');
+defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/test-helper');
 defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment');
 defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment2');
 defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment3');
