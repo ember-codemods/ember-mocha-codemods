cc54f2f38971daaee6985d0759d00df1fc46aa06
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-24 20:15:19 +0100

    Inline beautifyImports() function
    Proper solution would be to use something like rollup, but this solution seems fine for now...

--- new-testing-api.js
+++ new-testing-api.js
@@ -1,5 +1,3 @@
-import beautifyImports from './helpers/beautify-imports';
-
 export default function(file, api, options) {
   const j = api.jscodeshift;
 
@@ -68,4 +66,32 @@ export default function(file, api, options) {
     });
 
   return beautifyImports(root.toSource(printOptions));
+}
+
+/**
+ * imported from https://github.com/tomdale/ember-modules-codemod
+ */
+function beautifyImports(source) {
+  return source.replace(/\bimport.+from/g, (importStatement) => {
+    let openCurly = importStatement.indexOf('{');
+
+    // leave default only imports alone
+    if (openCurly === -1) { return importStatement; }
+
+    if (importStatement.length > 50) {
+      // if the segment is > 50 chars make it multi-line
+      let result = importStatement.slice(0, openCurly + 1);
+      let named = importStatement
+        .slice(openCurly + 1, -6).split(',')
+        .map(name => `\n  ${name.trim()}`);
+
+      return result + named.join(',') + '\n} from';
+    } else {
+      // if the segment is < 50 chars just make sure it has proper spacing
+      return importStatement
+        .replace(/,\s*/g, ', ') // ensure there is a space after commas
+        .replace(/\{\s*/, '{ ')
+        .replace(/\s*\}/, ' }');
+    }
+  });
 }
