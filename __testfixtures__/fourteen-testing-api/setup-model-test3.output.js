79b17a1038503b5d5d3c6ec065695292d0647ade
John Bryson <john.bryson@resonate.com>
2020-02-14 10:24:55 -0500

    Add test for wrapping model.createRecord with runloop

---
+++ __testfixtures__/fourteen-testing-api/setup-model-test3.output.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+
+describe('Unit | Model | rental', function() {
+  setupTest();
+  // Replace this with your real tests.
+  it('exists', function() {
+    let model = Ember.run(
+      () => this.owner.lookup('service:store').createRecord('rental', {num: 5, obj: {key: 'val'}, arr: [1,'two']})
+    );
+    expect(model).to.be.ok;
+  });
+});
