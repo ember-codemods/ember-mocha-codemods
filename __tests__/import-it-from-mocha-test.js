060933187ab20b2c05b8613a8f1b61cd3e64532e
John Bryson <john.bryson@resonate.com>
2020-02-14 13:54:10 -0500

    Standardizing testing methodology which remove the need for hardcoded test names

--- __tests__/import-it-from-mocha-test.js
+++ __tests__/import-it-from-mocha-test.js
@@ -1,10 +1,40 @@
+/* eslint-env: node */
 'use strict';
 
-const defineTest = require('jscodeshift/dist/testUtils').defineTest;
+const fs = require('fs');
+const path = require('path');
 
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/basic');
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/test-helper');
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment');
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment2');
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-comment3');
-defineTest(__dirname, 'import-it-from-mocha', {}, 'import-it-from-mocha/with-other-imports');
+const runInlineTest = require('jscodeshift/dist/testUtils').runInlineTest;
+const ImportItFromMochaTransform = require('../import-it-from-mocha');
+const fixtureFolder = `${__dirname}/../__testfixtures__/import-it-from-mocha`;
+
+describe('import-it-from-mocha', function() {
+  fs
+    .readdirSync(fixtureFolder)
+    .filter(filename => /\.input\.js$/.test(filename))
+    .forEach(filename => {
+      let testName = filename.replace('.input.js', '');
+      let inputPath = path.join(fixtureFolder, `${testName}.input.js`);
+      let outputPath = path.join(fixtureFolder, `${testName}.output.js`);
+
+      describe(testName, function() {
+        it('transforms correctly', function() {
+          runInlineTest(
+            ImportItFromMochaTransform,
+            {},
+            { source: fs.readFileSync(inputPath, 'utf8') },
+            fs.readFileSync(outputPath, 'utf8')
+          );
+        });
+
+        it('is idempotent', function() {
+          runInlineTest(
+            ImportItFromMochaTransform,
+            {},
+            { source: fs.readFileSync(outputPath, 'utf8') },
+            fs.readFileSync(outputPath, 'utf8')
+          );
+        });
+      });
+    });
+});
